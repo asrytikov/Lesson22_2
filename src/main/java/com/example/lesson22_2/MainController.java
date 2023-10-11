9f47c04f36c43330ee7dfe947a833dfc85d7437d
asrytikov <asrytikov@gmail.com>
2023-10-11 21:32:25 +0400

    Lesson22

--- src/main/java/com/example/lesson22_2/MainController.java
+++ src/main/java/com/example/lesson22_2/MainController.java
@@ -1,2 +1,29 @@
-package com.example.lesson22_2;public class MainController {
+package com.example.lesson22_2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    /*@RequestMapping("/home")
+    public String home(@RequestParam(required = false) String color,
+                       @RequestParam(required = false) String name,
+                       Model model){
+        model.addAttribute("username", name);
+        model.addAttribute("color", color);
+        return "home.html";
+    }*/
+
+    @RequestMapping("/home/{color}")
+    public String home(@PathVariable String color,
+                       Model model){
+        model.addAttribute("username", "Katya");
+        model.addAttribute("color", color);
+        return "home.html";
+    }
+
 }
